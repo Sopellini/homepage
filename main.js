4446dc551b409d29797ea3f46030c0bb4bb6574d
Sopellini <soping87@gmail.com>
2023-08-31 23:13:43 +0200

    added greeting js function

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 const mainHeader = document.querySelector('.header')
 mainHeader.innerHTML = '<h1>Cześć Gościu! To moja strona domowa.</h1>'
+
+const greet = (name, age) => {
+    const result = `Siema ${name}! Fajnie że masz ${age} lat!`;
+    console.log(result);
+}
+greet('Luk', 35);
